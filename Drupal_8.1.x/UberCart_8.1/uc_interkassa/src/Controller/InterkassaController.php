53039b4a41207c2e28ce180fa77abfc3f0d28418
CMSInterkassa <cms@interkassa.com>
2016-10-20 17:45:49 +0300

    Drupal 7.5(UC) 8.1(UC) 8.2(UC)

---
+++ Drupal_8.1.x/UberCart_8.1/uc_interkassa/src/Controller/InterkassaController.php
@@ -0,0 +1,194 @@
+<?php
+
+/**
+ * Модуль разработан в компании GateOn предназначен для CMS Drupal 8.1.x + Ubercart 4
+ * Сайт разработчикa: www.gateon.net
+ * E-mail: www@smartbyte.pro
+ * Версия: 1.1
+ */
+
+namespace Drupal\uc_interkassa\Controller;
+
+use Drupal\Component\Utility\SafeMarkup;
+use Drupal\Component\Utility\Unicode;
+use Drupal\Core\Controller\ControllerBase;
+use Drupal\uc_cart\CartManagerInterface;
+use Drupal\uc_order\Entity\Order;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
+
+
+class InterkassaController extends ControllerBase
+{
+
+    /**
+     * @var \Drupal\uc_cart\CartManager
+     */
+    protected $cartManager;
+
+    /**
+     * @param \Drupal\uc_cart\CartManagerInterface $cart_manager
+     *   The cart manager.
+     */
+    public function __construct(CartManagerInterface $cart_manager)
+    {
+        $this->cartManager = $cart_manager;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public static function create(ContainerInterface $container)
+    {
+        return new static(
+            $container->get('uc_cart.manager')
+        );
+    }
+
+    public function complete($cart_id = 0, Request $request)
+    {
+
+        \Drupal::logger('uc_interkassa')->notice('Получено оповещение о заказе @order_id.', ['@order_id' => SafeMarkup::checkPlain
+        ($request->request->get('ik_pm_no'))]);
+
+        $this->wrlog('complete');
+
+        $request_order_id = $request->request->get('ik_pm_no');
+        $order = Order::load($request_order_id);
+
+        $plugin = \Drupal::service('plugin.manager.uc_payment.method')->createFromOrder($order);
+
+        $configuration = $plugin->getConfiguration();
+
+        $address = $order->getAddress('billing');
+        $address->street1 = $request->request->get('street_address');
+        $address->street2 = $request->request->get('street_address2');
+        $address->city = $request->request->get('city');
+        $address->postal_code = $request->request->get('zip');
+        $address->phone = $request->request->get('phone');
+        $address->zone = $request->request->get('state');
+        $address->country = $request->request->get('country');
+        $order->setAddress('billing', $address);
+        $order->setPaymentMethodId($order->getPaymentMethodId());
+
+        uc_order_comment_save($order->id(), 0, $this->t('Заказ был сделан на сайте с помощью Интеркасса.'), 'admin');
+
+        switch ($request->request->get('ik_inv_st')) {
+            case 'canceled':
+                $order->setStatusId('canceled')->save();
+                $comment = $this->t('Заказ был отменен пользователем');
+                uc_payment_enter($order->id(), 'interkassa', $request->request->get('total'), 0, NULL, $comment);
+                drupal_set_message($this->t('Заказ был отменен пользователем'));
+                uc_order_comment_save($order->id(), 0, $this->t('Заказ не был оплачен с помощью Интеркассы.'), 'admin');
+                return array(
+                    '#theme' => 'uc_cart_complete_sale',
+                    '#message' => array('#markup' => 'Заказ был отменен пользователем'),
+                    '#order' => $order,
+                );
+                break;
+            case 'waitAccept':
+                $order->setStatusId('pending')->save();
+                $comment = $this->t('Заказ не был оплачен');
+                uc_payment_enter($order->id(), 'interkassa', $request->request->get('total'), 0, NULL, $comment);
+                drupal_set_message($this->t('Заказ не был оплачен'));
+                uc_order_comment_save($order->id(), 0, $this->t('Заказ не был оплачен с помощью Интеркассы.'), 'admin');
+                return array(
+                    '#theme' => 'uc_cart_complete_sale',
+                    '#message' => array('#markup' => 'Заказ не был оплачен'),
+                    '#order' => $order,
+                );
+                break;
+            case 'success':
+                $comment = $this->t('Заказ был оплачен, пользователь вернулся на сайт');
+                uc_payment_enter($order->id(), 'interkassa', $request->request->get('total'), 0, NULL, $comment);
+                drupal_set_message($this->t('Ваш заказ был обработан с помощью Интеркассы'));
+                uc_order_comment_save($order->id(), 0, $this->t('Заказ оплачен с помощью Интеркассы.'), 'admin');
+               return $this->cartManager->completeSale($order);
+                break;
+
+        }
+    }
+
+    public function notification(Request $request)
+    {
+        $values = $request->request;
+        \Drupal::logger('uc_interkassa')->notice('Получено оповещение о заказе с следующими данными: @data', ['@data' => print_r($values->all(), TRUE)]);
+
+        $this->wrlog('notification');
+//        $this->wrlog('#####################START#####################');
+//        foreach ($values as $key => $value) {
+//            $this->wrlog($key . '=>' . $value);
+//        }
+//        $this->wrlog('######################END#######################');
+
+        $request_order_id = $values->get('ik_pm_no');
+
+        if (isset($request_order_id)) {
+
+            $this->wrlog('order id ok');
+
+            $order_id = $values->get('ik_pm_no');
+            $order = Order::load($order_id);
+
+            $plugin = \Drupal::service('plugin.manager.uc_payment.method')->createFromOrder($order);
+
+            $configuration = $plugin->getConfiguration();
+
+
+            $this->wrlog($values->get('ik_inv_st'));
+
+            if ($values->get('ik_inv_st') == 'success') {
+
+                $this->wrlog('success');
+
+                if ($values->get('ik_pw_via') && $values->get('ik_pw_via') == 'test_interkassa_test_xts') {
+                    $secret_key = $configuration['test_key'];
+                } else {
+                    $secret_key = $configuration['secret_key'];
+                }
+
+                $request_sign = $values->get('ik_sign');
+
+                $dataSet = [];
+
+                foreach ($values as $key => $value) {
+                    if (!preg_match('/ik_/', $key)) continue;
+                    $dataSet[$key] = $value;
+                }
+
+                unset($dataSet['ik_sign']);
+                ksort($dataSet, SORT_STRING);
+                array_push($dataSet, $secret_key);
+                $signString = implode(':', $dataSet);
+                $sign = base64_encode(md5($signString, true));
+
+
+                if ($request_sign != $sign) {
+                    \Drupal::logger('uc_interkassa')->notice('Подписи не совпадают!: @data', ['@data' => print_r($values->get('ik_sign'), TRUE)]);
+                    $this->wrlog('Подписи не совпадают!');
+                    $order->setStatusId('canceled')->save();
+                    die('Hash Incorrect');
+
+                } else {
+                    $this->wrlog('Подписи совпадают!');
+                    $order->setStatusId('payment_received')->save();
+                }
+            }
+
+        } else {
+
+            $this->wrlog('params didnt match');
+        }
+
+    }
+
+    public function wrlog($content)
+    {
+        $file = 'log.txt';
+        $doc = fopen($file, 'a');
+        file_put_contents($file, PHP_EOL . $content, FILE_APPEND);
+        fclose($doc);
+    }
+
+}
---
+++ Drupal_8.1.x/UberCart_8.1/uc_interkassa/src/Plugin/Ubercart/PaymentMethod/Interkassa.php
@@ -0,0 +1,231 @@
+<?php
+/**
+ * Модуль разработан в компании GateOn предназначен для CMS Drupal 8.1.x + Ubercart 4
+ * Сайт разработчикa: www.gateon.net
+ * E-mail: www@smartbyte.pro
+ * Версия: 1.1
+ */
+namespace Drupal\uc_interkassa\Plugin\Ubercart\PaymentMethod;
+
+use Drupal\Component\Utility\Unicode;
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Url;
+use Drupal\uc_order\OrderInterface;
+use Drupal\uc_payment\OffsitePaymentMethodPluginInterface;
+use Drupal\uc_payment\PaymentMethodPluginBase;
+
+/**
+ * Defines the Interkassa payment method.
+ *
+ * @UbercartPaymentMethod(
+ *   id = "interksassa",
+ *   name = @Translation("Interksassa"),
+ *   redirect = "\Drupal\uc_interkassa\Form\InterkassaForm",
+ * )
+ */
+class Interkassa extends PaymentMethodPluginBase implements OffsitePaymentMethodPluginInterface
+{
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getDisplayLabel($label)
+    {
+        $build['#attached']['library'][] = 'uc_interkassa/interkassa.styles';
+        $build['label'] = array(
+            '#plain_text' => $label,
+            '#suffix' => '<br />',
+        );
+        $build['image'] = array(
+            '#theme' => 'image',
+            '#uri' => drupal_get_path('module', 'uc_interkassa') . '/images/interkassa_logo.gif',
+            '#alt' => $this->t('Interkassa'),
+            '#attributes' => array('class' => array('uc-interkassa-logo')),
+        );
+
+        return $build;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function defaultConfiguration()
+    {
+        return [
+            'check' => FALSE,
+            'checkout_type' => 'dynamic',
+            'notification_url' => '',
+            'sid' => '',
+            'secret_key' => '',
+            'test_key' => ''
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function buildConfigurationForm(array $form, FormStateInterface $form_state)
+    {
+        $form['sid'] = array(
+            '#type' => 'textfield',
+            '#title' => $this->t('Interkassa ID'),
+            '#description' => $this->t('ID of your Interkassa'),
+            '#default_value' => $this->configuration['sid'],
+            '#size' => 40,
+        );
+        $form['secret_key'] = array(
+            '#type' => 'textfield',
+            '#title' => $this->t('Секретный ключ'),
+            '#description' => $this->t('Взять с настроек кассы "Безопасность" -> "Секретный ключ" '),
+            '#default_value' => $this->configuration['secret_key'],
+            '#size' => 40,
+        );
+        $form['test_key'] = array(
+            '#type' => 'textfield',
+            '#title' => $this->t('Тестовый ключ'),
+            '#description' => $this->t('Взять с настроек кассы "Безопасность" -> "Тестовый ключ" '),
+            '#default_value' => $this->configuration['test_key'],
+            '#size' => 40,
+        );
+
+        $form['checkout_type'] = array(
+            '#type' => 'radios',
+            '#title' => $this->t('Checkout type'),
+            '#options' => array(
+                'dynamic' => $this->t('Dynamic checkout (user is redirected to Interkassa)'),
+                'direct' => $this->t('Direct checkout (payment page opens in iframe popup)'),
+            ),
+            '#default_value' => $this->configuration['checkout_type'],
+        );
+        $form['notification_url'] = array(
+            '#type' => 'url',
+            '#title' => $this->t('URL Уведомления'),
+            '#description' => $this->t('Pass this URL to the <a href=":help_url">instant notification settings</a> parameter in your Interkassa account. This way, any refunds or failed fraud reviews will automatically cancel the Ubercart order.', [':help_url' => Url::fromUri('https://www.interkassa.com/documentation-sci/')->toString()]),
+            '#default_value' => Url::fromRoute('uc_interkassa.notification', [], ['absolute' => TRUE])->toString(),
+            '#attributes' => array('readonly' => 'readonly'),
+        );
+
+        return $form;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function submitConfigurationForm(array &$form, FormStateInterface $form_state)
+    {
+        $this->configuration['notification_url'] = $form_state->getValue('notification_url');
+        $this->configuration['sid'] = $form_state->getValue('sid');
+        $this->configuration['secret_key'] = $form_state->getValue('secret_key');
+        $this->configuration['test_key'] = $form_state->getValue('test_key');
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function cartDetails(OrderInterface $order, array $form, FormStateInterface $form_state)
+    {
+        $build = array();
+        $session = \Drupal::service('session');
+        if ($this->configuration['check']) {
+            $build['pay_method'] = array(
+                '#type' => 'select',
+                '#title' => $this->t('Select your payment type:'),
+                '#default_value' => $session->get('pay_method') == 'CK' ? 'CK' : 'CC',
+                '#options' => array(
+                    'CC' => $this->t('Credit card'),
+                    'CK' => $this->t('Online check'),
+                ),
+            );
+            $session->remove('pay_method');
+        }
+
+        return $build;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function cartProcess(OrderInterface $order, array $form, FormStateInterface $form_state)
+    {
+        $session = \Drupal::service('session');
+        if (NULL != $form_state->getValue(['panes', 'payment', 'details', 'pay_method'])) {
+            $session->set('pay_method', $form_state->getValue(['panes', 'payment', 'details', 'pay_method']));
+        }
+        return TRUE;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function cartReviewTitle()
+    {
+        if ($this->configuration['check']) {
+            return $this->t('Credit card/eCheck');
+        } else {
+            return $this->t('Credit card');
+        }
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function buildRedirectForm(array $form, FormStateInterface $form_state, OrderInterface $order = NULL)
+    {
+        $address = $order->getAddress('billing');
+        if ($address->country) {
+            $country = \Drupal::service('country_manager')->getCountry($address->country)->getAlpha3();
+        } else {
+            $country = '';
+        }
+        
+        $data = array(
+            'ik_co_id' => $this->configuration['sid'],
+            'ik_pm_no' => $order->id(),
+            'ik_desc' => '#' . $order->id(),
+            'ik_am' => uc_currency_format($order->getTotal(), FALSE, FALSE, '.'),
+            'ik_cur' => $order->getCurrency(),
+            'ik_ia_u' => Url::fromRoute('uc_interkassa.notification', ['cart_id' => \Drupal::service('uc_cart.manager')->get()->getId()], ['absolute' => TRUE])->toString(),
+            'ik_fal_u' => Url::fromRoute('uc_interkassa.complete', ['cart_id' => \Drupal::service('uc_cart.manager')->get()->getId()], ['absolute' => TRUE])->toString(),
+            'ik_suc_u' => Url::fromRoute('uc_interkassa.complete', ['cart_id' => \Drupal::service('uc_cart.manager')->get()->getId()], ['absolute' => TRUE])->toString(),
+            'ik_pnd_u' => Url::fromRoute('uc_interkassa.complete', ['cart_id' => \Drupal::service('uc_cart.manager')->get()->getId()], ['absolute' => TRUE])->toString()
+
+        );
+
+        $dataSet = $data;
+        ksort($dataSet, SORT_STRING);
+        array_push($dataSet, $this->configuration['secret_key']);
+        $signString = implode(':', $dataSet);
+        $sign = base64_encode(md5($signString, true));
+
+        $data['ik_sign'] = $sign;
+        
+        $i = 0;
+        foreach ($order->products as $product) {
+            $i++;
+            $data['li_' . $i . '_type'] = 'product';
+            $data['li_' . $i . '_name'] = $product->title->value; // @todo: HTML escape and limit to 128 chars
+            $data['li_' . $i . '_quantity'] = $product->qty->value;
+            $data['li_' . $i . '_product_id'] = $product->model->value;
+            $data['li_' . $i . '_price'] = uc_currency_format($product->price->value, FALSE, FALSE, '.');
+        }
+
+        if ('direct' == $this->configuration['checkout_type']) {
+            $form['#attached']['library'][] = 'uc_interkassa/interkasssa.direct';
+        }
+
+        $form['#action'] = "https://sci.interkassa.com/";
+
+        foreach ($data as $name => $value) {
+            $form[$name] = array('#type' => 'hidden', '#value' => $value);
+        }
+
+        $form['actions'] = array('#type' => 'actions');
+        $form['actions']['submit'] = array(
+            '#type' => 'submit',
+            '#value' => $this->t('Submit order'),
+        );
+
+        return $form;
+    }
+
+}
